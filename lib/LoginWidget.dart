6a8f419564dfea2c622b3dd6c0fdba630fe53706
ottuck <vison901@gmail.com>
2024-03-19 17:23:12 +0900

    로그인 위젯 dart파일로 변경(에러수정)

---
+++ lib/LoginWidget.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:flutter_study1/SignUpPage.dart';
+import 'package:flutter_study1/FirebaseAuthService.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({super.key});
+
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+//Print log messages to the console
+final Logger logger = Logger();
+
+class _LoginWidgetState extends State<LoginWidget> {
+  final _formKey = GlobalKey<FormState>();
+  final FirebaseAuthService _authService = FirebaseAuthService();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('로그인'),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: '이메일',
+                  hintText: '이메일을 입력하세요',
+                  border: OutlineInputBorder(),
+                ),
+                controller: _emailController,
+                validator: (value) {
+                  if (value!.isEmpty || !value.contains('@')) {
+                    return '유효한 이메일을 입력하세요';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: '비밀번호',
+                  hintText: '비밀번호를 입력하세요',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+                controller: _passwordController,
+                validator: (value) {
+                  if (value!.isEmpty || value.length < 6) {
+                    return '비밀번호는 6자 이상이어야 합니다.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  logger.e('로그인 버튼을 눌렀습니다.');
+                  _authService.signIn(
+                      _emailController.text, _passwordController.text);
+                },
+                child: const Text('로그인'),
+              ),
+              const SizedBox(height: 20.0),
+              TextButton(
+                onPressed: () {
+                  logger.e('회원가입 버튼을 눌렀습니다.');
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignUpPage()));
+                },
+                child: const Text('회원가입'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
